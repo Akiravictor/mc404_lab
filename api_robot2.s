cd2aa829d3248552acc0d58d6635d2b6cfeda49a
Victor Akira <victor.akira.hs@gmail.com>
2015-06-21 03:24:54 -0300

    Pull to my branch

--- api_robot2.s
+++ api_robot2.s
@@ -1,83 +1,76 @@
 #include "api_robot2.h"
     
-    .global set_motor_speed
-    .global set_motors_speed
-    .global read_sonar
-    .global read_sonars
-    .global set_alarm
-    .global get_time
-    .global set_time
+	.global set_motor_speed
+	.global set_motors_speed
+	.global read_sonar
+	.global read_sonars
+	.global set_alarm
+	.global get_time
+	.global set_time
    
    
 .align 4
-@@inicio da programação das syscalls
 
-@@ MOTORES
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@			MOTORS			@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-@ ainda falta definir o que é uma velocidade inválida  -> inválida é maior que 63
-    
-    set_motor_speed:
-        stmfd sp!, {r4-r6, r10-r11, lr} @ Save the callee-save registers
-                                        @ and the return address.
+
+set_motor_speed:
+	stmfd sp!, {r4-r6, r10-r11, lr} @ Save the callee-save registers
+		                            @ and the return address.
         
-        checaMotor:
-            cmp r0, #1
-            bls checaVelocidade
-              @else
-                mov r0,#-1    
-                ldmfd sp!, {r4-r6, r10-r11, pc}
+	checaMotor:
+		cmp r0, #1
+		bls checaVelocidade
+		@ If r0 is higher than #1, return -1 (Invalid id)
+		mov r0,#-1    
+		ldmfd sp!, {r4-r6, r10-r11, pc}
 
-        checaVelocidade:
-        @else
-          cmp r1,#63
-          bls liga_motor
-          mov r0,#-2
-          
-        ldmfd sp!, {r4-r6, r10-r11, pc}
+	checaVelocidade:
+		cmp r1,#63
+		bls liga_motor
+		@ If speed is higher than #63, return -2 (Invalid speed)
+		mov r0,#-2
+		ldmfd sp!, {r4-r6, r10-r11, pc}
 
-        liga_motor:
-            mov r3,#9               @carrega a chamada do motor             
-            mov r7, r3            
-            svc 0x0                 @ Faz a chamada da syscall.
-    @Lembrar de colocar o 0 em R0 após setar a velocidade
-        ldmfd sp!, {r4-r6, r10-r11, pc}
+	liga_motor:
+		mov r3,#9               @ Load motor call
+		mov r7, r3            
+		svc 0x0                 @ Make syscall.
 
+	mov r0,#0
+	ldmfd sp!, {r4-r6, r10-r11, pc}
 
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-    set_motors_speed:
-        stmfd sp!, {r4-r6, r10-r11, lr} @ Save the callee-save registers
-                        @ and the return address.
-                        
-                        
-        @ainda falta definir o que são velocidades válidas        
+set_motors_speed:
+    stmfd sp!, {r4-r6, r10-r11, lr} @ Save the callee-save registers
+					                @ and the return address.
         
-        cmp r0,#70               @verifica o valor da velocidade do motor0
-        bls verificaMotor_2
-        mov r0,#-1
-        ldmfd sp!, {r4-r6, r10-r11, pc}
-        
-        verificaMotor_2:
-
-        cmp r1,#70               @verifica o valor da velocidade do motor1
-        bls liga_motores
-        mov r0,#-2
-        ldmfd sp!, {r4-r6, r10-r11, pc}    
-        
-        liga_motores:           @caso os valroes sejam corretos, executa a chamada da syscall
-        mov r7, #10             @ Identifica a syscall 124 (write_motors).
-        svc 0x0                 @ Faz a chamada da syscall.
-
-    @setar r0 em 0 dentro do soul;
-
+    cmp r0,#63
+    bls verificaMotor_2
+	@ If speed for motor0 is higher than #63, return -1 (Invalid speed for motor0)
+    mov r0,#-1
     ldmfd sp!, {r4-r6, r10-r11, pc}
+        
+    verificaMotor_2:
+		cmp r1,#63
+		bls liga_motores
+		@ If speed for motor1 is higher than #63, return -2 (Invalid speed for motor1)
+		mov r0,#-2
+		ldmfd sp!, {r4-r6, r10-r11, pc}    
+        
+    liga_motores:
+		mov r7, #10             @ Load motors call
+		svc 0x0                 @ Make syscall.
+
+	mov r0,#0
+	ldmfd sp!, {r4-r6, r10-r11, pc}
     
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        
-@@ SONARES
-        
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@			SONARES			@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
     read_sonar:
         stmfd sp!, {r4-r6, r10-r11, lr}
@@ -94,12 +87,10 @@
             svc 0x0                  
             
         ldmfd sp!, {r4-r6, r10-r11, pc}
-     
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-@@ SYSTEM
-
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@			SYSTEM			@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
     set_alarm:
         stmfd sp!, {r4-r6, r10-r11, lr}
@@ -109,7 +100,7 @@
         ldmfd sp!, {r4-r6, r10-r11, pc}
     
 
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     get_time:
         stmfd sp!, {r4-r6, r10-r11, lr}
         mov r7, #11
@@ -118,15 +109,12 @@
         ldmfd sp!, {r4-r6, r10-r11, pc}
     
 
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
     set_time:
         stmfd sp!, {r4-r6, r10-r11, lr}
         mov r7, #12
         svc 0x0
             
         ldmfd sp!, {r4-r6, r10-r11, pc}
-    
-
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
 
